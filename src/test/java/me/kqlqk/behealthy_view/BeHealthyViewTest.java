7721b0c7dcf0a6ed1eae5b65126c356034bf09df
kqlqk <kononchuk99999@gmail.com>
2022-10-06 16:50:12 +0200

    init

---
+++ src/test/java/me/kqlqk/behealthy_view/BeHealthyViewTest.java
@@ -0,0 +1,13 @@
+package me.kqlqk.behealthy_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeHealthyViewTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
