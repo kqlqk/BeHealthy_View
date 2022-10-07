57d5d40af754111164dd7daa32f859e3b9d169dc
kqlqk <kononchuk99999@gmail.com>
2022-10-07 12:20:34 +0200

    new endpoints
    * was added new endpoints GET '/login', POST '/login'

--- src/main/java/me/kqlqk/behealthy/view/controller/GuestController.java
+++ src/main/java/me/kqlqk/behealthy/view/controller/GuestController.java
@@ -1,7 +1,11 @@
 package me.kqlqk.behealthy.view.controller;
 
+import me.kqlqk.behealthy.view.dto.LoginDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GuestController {
@@ -9,6 +13,19 @@ public class GuestController {
     @GetMapping
     public String getMainPage() {
         return "guest/MainPage";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        model.addAttribute("loginDTO", new LoginDTO());
+
+        return "guest/LoginPage";
+    }
+
+    @PostMapping("/login")
+    public String logIn(@ModelAttribute("loginDTO") LoginDTO loginDTO) {
+        System.out.println(loginDTO.getEmail() + ":" + loginDTO.getPassword());
+        return "redirect:/me";
     }
 
 }
---
+++ src/main/java/me/kqlqk/behealthy/view/dto/LoginDTO.java
@@ -0,0 +1,9 @@
+package me.kqlqk.behealthy.view.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String email;
+    private String password;
+}
