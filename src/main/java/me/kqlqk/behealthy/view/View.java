2ea04b0bfc443b6e9a7bd92d2d8d917a9b689e30
kqlqk <kononchuk99999@gmail.com>
2022-10-25 21:31:46 +0200

    bugfix

--- src/main/java/me/kqlqk/behealthy/view/View.java
+++ src/main/java/me/kqlqk/behealthy/view/View.java
@@ -3,10 +3,12 @@ package me.kqlqk.behealthy.view;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class View extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
